dbcef4c6fc95d50562d59160b703e28e65024d38
Mohammad Salam <m.salam@houya.io>
2023-12-03 19:35:05 +0100

    Add models and home.vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="container">
+    <h1 class="alert alert-success">Hello home page!</h1>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import Vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        Vue(),
+    ],
+});
