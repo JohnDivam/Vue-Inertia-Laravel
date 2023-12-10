5cbdcd14b62b1e6e431050df42d88893179a99bd
Mohammad Salam <m.salam@houya.io>
2023-12-10 21:10:41 +0100

    Add modal

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import Vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,12 @@ export default defineConfig({
             input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
-        Vue(),
+        vue({
+            template: {
+              compilerOptions: {
+                isCustomElement: (tag) => ['md-linedivider'].includes(tag),
+              }
+            }
+          }),
     ],
 });
